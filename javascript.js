50acf0944cb80fd753d778bfe214038e37b6f1b4
Hugo Leung <132837475+hugounique@users.noreply.github.com>
2024-02-24 22:32:53 +0800

    Implement player add value function and avoid assignment of value on the same spot

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
 //function to create a board
 //function for checking if anyone wins or tie
+//  check for same values in one row, one column and 
 //function for reprinting the board
 
-const gameboard = (() => {
+function Gameboard () {
     const rows = 3;
     const columns = 3;
     const board = [];
@@ -16,22 +17,26 @@ const gameboard = (() => {
 
     const getBoard = () => board;
 
+    const addValue = (row, column, player) => {
+        let targetCell = board[row][column];
+           targetCell.addValue(player); 
+
+    }
+
     const printBoard = () => {
-        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
         console.log(boardWithCellValues);
     }
 
-    return { getBoard, printBoard};
+    return {getBoard, addValue, printBoard};
 
-})();
-
-gameboard.printBoard();
+};
 
 //Object for cell
 //1. able to get value
 //2. able to add value
 function Cell() {
-    let value = '';
+    let value;
 
     const addValue = (player) => {
         value = player;
@@ -46,10 +51,62 @@ function Cell() {
 }
 
 
-//factory functions for creating players
+// factory functions for creating players(done)
 //store their name, value
 //function for adding their symbol
-//
+
+function GameController(playerOneName = "Player One",
+playerTwoName = "PLayer Two") {
+    const board = Gameboard();
+
+    const players = [
+        {
+            name: playerOneName,
+            value: 'O'
+        },
+        {
+            name: playerTwoName,
+            value: 'X'
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+      };
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`);
+      };
+
+    const playRound = (row, column) => {
+         const selectedCell = board.getBoard()[row][column];
+        if (selectedCell.getValue() !== undefined) {
+        console.log('Oops... That cell is already occupied.');
+        printNewRound();
+        return; // Exit the function early
+    }
+        
+        board.addValue(row, column, getActivePlayer().value);
+        //check win logic here
+        switchPlayerTurn();
+        printNewRound();
+    };
+
+    printNewRound();
+
+    return {
+        playRound,
+        getActivePlayer
+    };
+}
+
+const game = GameController();
+
 
 //1.create a board
 //2.decide who starts first
@@ -58,3 +115,6 @@ function Cell() {
 //4.check to see if anyone wins every time a value to added
 //5.switch round if the game hasn't ended
 //6. If win/tie, reprint the board and start again
+
+//1. check if the value of the arrays has changed
+//2.  
